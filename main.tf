4caa6f7f4084ea90590b5a6210cf3b212df192b9
garyrayat <59029064+garyrayat@users.noreply.github.com>
2026-04-19 01:49:42 -0500

    Add static public IP resource to main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "example" {
   name     = "rg-governance-demo"
   location = "East US"
 }
+
+resource "azurerm_public_ip" "bad_example" {
+  name                = "bad-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.governance_lab.name
+  allocation_method   = "Static"
+}
