75e44d4c6cc6d11b12107a6e0197283712897cb2
garyrayat <59029064+garyrayat@users.noreply.github.com>
2026-04-19 01:56:02 -0500

    Add azurerm_public_ip resource configuration

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "azurerm_public_ip" "bad_example" {
   resource_group_name = azurerm_resource_group.governance_lab.name
   allocation_method   = "Static"
 }
+
+resource "azurerm_public_ip" "bad_example" {
+  name                = "bad-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.governance_lab.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
